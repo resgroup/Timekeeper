5f0d88f8224af0da03cbb5d78a57e38412bf25ff
Cedd Burge <cedd.burge@res-group.com>
2019-10-31 13:20:12 +0000

    Add files that were in Peter Arnolds zip file, but not committed to his git repo

---
+++ RES-Timekeeper/OptionsForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace RES_Timekeeper
+{
+    partial class OptionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._btnOK = new System.Windows.Forms.Button();
+            this._btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this._cbTimeLevels = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // _btnOK
+            // 
+            this._btnOK.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this._btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this._btnOK.Location = new System.Drawing.Point(127, 52);
+            this._btnOK.Name = "_btnOK";
+            this._btnOK.Size = new System.Drawing.Size(75, 23);
+            this._btnOK.TabIndex = 0;
+            this._btnOK.Text = "OK";
+            this._btnOK.UseVisualStyleBackColor = true;
+            // 
+            // _btnCancel
+            // 
+            this._btnCancel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this._btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this._btnCancel.Location = new System.Drawing.Point(208, 52);
+            this._btnCancel.Name = "_btnCancel";
+            this._btnCancel.Size = new System.Drawing.Size(75, 23);
+            this._btnCancel.TabIndex = 1;
+            this._btnCancel.Text = "Cancel";
+            this._btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time Tracking Level";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // _cbTimeLevels
+            // 
+            this._cbTimeLevels.FormattingEnabled = true;
+            this._cbTimeLevels.Items.AddRange(new object[] {
+            "See Constructor"});
+            this._cbTimeLevels.Location = new System.Drawing.Point(127, 12);
+            this._cbTimeLevels.Name = "_cbTimeLevels";
+            this._cbTimeLevels.Size = new System.Drawing.Size(156, 21);
+            this._cbTimeLevels.TabIndex = 3;
+            // 
+            // OptionsForm
+            // 
+            this.AcceptButton = this._btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this._btnCancel;
+            this.ClientSize = new System.Drawing.Size(295, 87);
+            this.Controls.Add(this._cbTimeLevels);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._btnCancel);
+            this.Controls.Add(this._btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OptionsForm";
+            this.Text = "Options";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _btnOK;
+        private System.Windows.Forms.Button _btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox _cbTimeLevels;
+    }
+}
---
+++ RES-Timekeeper/OptionsForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RES_Timekeeper
+{
+    public partial class OptionsForm : Form
+    {
+        public enum TimeTrackingLevels { M15, M30, H1, M1};
+
+        public OptionsForm()
+        {
+            InitializeComponent();
+
+            _cbTimeLevels.Items.Clear();
+            _cbTimeLevels.Items.Add("15 Minutes");
+            _cbTimeLevels.Items.Add("30 Minutes");
+            _cbTimeLevels.Items.Add("1 Hour");
+#if DEBUG
+            _cbTimeLevels.Items.Add("1 Minute (DEBUG)");
+#endif
+        }
+
+        public TimeTrackingLevels SelectedTimeTrackingLevel
+        {
+            get
+            {
+                return (TimeTrackingLevels)(_cbTimeLevels.SelectedIndex);
+            }
+
+            set
+            {
+                _cbTimeLevels.SelectedIndex = (int)value;
+            }
+        }
+    }
+}
