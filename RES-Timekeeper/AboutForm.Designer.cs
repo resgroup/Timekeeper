d5f5928748bf1752038d6d26ca4b85647367f20e
Cedd Burge <cedd.burge@res-group.com>
2019-11-05 15:07:16 +0000

    Remove res logo

--- RES-Timekeeper/AboutForm.Designer.cs
+++ RES-Timekeeper/AboutForm.Designer.cs
@@ -27,24 +27,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-            this.pictureBox = new System.Windows.Forms.PictureBox();
             this.labelVersion = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox
-            // 
-            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
-            this.pictureBox.Location = new System.Drawing.Point(1, 1);
-            this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(200, 125);
-            this.pictureBox.TabIndex = 1;
-            this.pictureBox.TabStop = false;
             // 
             // labelVersion
             // 
-            this.labelVersion.Location = new System.Drawing.Point(207, 29);
+            this.labelVersion.Location = new System.Drawing.Point(12, 9);
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(194, 36);
             this.labelVersion.TabIndex = 22;
@@ -57,7 +45,6 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(413, 126);
             this.Controls.Add(this.labelVersion);
-            this.Controls.Add(this.pictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -66,14 +53,11 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "About RES-Timekeeper";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Label labelVersion;
 
 
