60008d0f41b584eb3890681e61a0aeefe4a955af
Cedd Burge <cedd.burge@res-group.com>
2019-11-05 15:08:27 +0000

    Remove res session logging

--- RES-Timekeeper/Program.cs
+++ RES-Timekeeper/Program.cs
@@ -16,45 +16,24 @@ namespace RES_Timekeeper
         // see http://odetocode.com/blogs/scott/archive/2004/08/20/the-misunderstood-mutex.aspx
         private static string appGuid = "4280EEF4-F185-421B-83ED-1CE92ECAD110";
 
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             using (Mutex mutex = new Mutex(false, appGuid))
             {
-#if DEBUG
-#else
                 if (!mutex.WaitOne(0, false))
                 {
                     //MessageBox.Show("Instance already running");
                     return;
                 }
-#endif
 
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
 
-                LogSession();
-                var mainForm = new MainForm();
+                // it looks like this line could be deleted, but it is required for some reason.
+                // If you don't have, the icon doesn't appear in the system tray
+                new MainForm(); 
                 Application.Run();
-            }
-        }
-
-
-        private static void LogSession()
-        {
-            // Attempt to log the session, but don't complain if we're not on the RES Network
-            try
-            {
-                SessionLogger.Log("Server=KL-SQL-001;DataBase=RESSoftware;Integrated Security=SSPI", softwareId: 1310);
-            }
-            catch (Exception ex)
-            {
-#if DEBUG
-                //MessageBox.Show("Error in session logging\n" + ex.ToString(), "Session logging", MessageBoxButtons.OK, MessageBoxIcon.Error);
-#endif
             }
         }
     }
